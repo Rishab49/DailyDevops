bb70933cd1a720f57039523651fb457dc0201b23
Rishab49 <25582966+Rishab49@users.noreply.github.com>
2026-01-03 20:05:45 +0530

    updated workflow to delete infra

--- challenge3/main.tf
+++ challenge3/main.tf
@@ -111,7 +111,6 @@ resource "aws_route_table" "route_table" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.ig.id
   }
-
   route {
     cidr_block = "10.0.0.0/16"
     gateway_id = "local"

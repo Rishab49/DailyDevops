ef5786e235c2972be98eeef77122f4b36aeab863
Rishab49 <25582966+Rishab49@users.noreply.github.com>
2026-01-03 19:32:28 +0530

    update tf

--- challenge3/main.tf
+++ challenge3/main.tf
@@ -1,18 +1,18 @@
-terraform{
-    required_providers{
-        aws = {
-            source = "hashicorp/aws"
-        }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
     }
+  }
 
-    backend "s3" {
+  backend "s3" {
     region = "us-east-1"
     bucket = "rajrishab-challenge2"
-    key = "state" 
+    key    = "state"
+  }
 }
-}
-resource "aws_vpc" "main"{
-    cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "subnet1" {
@@ -25,37 +25,43 @@ resource "aws_subnet" "subnet1" {
   }
 }
 
-resource "aws_instance" "instance1"{
-    ami = "ami-068c0051b15cdb816"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.subnet1.id
-    vpc_security_group_ids = [aws_security_group.SG1.id]
-    user_data = << EOF
-    sudo dnf update -y
-    sudo dnf install docker
-    cat << CONF > prom.yaml
-    global:
-        scrape_interval: 15s # How often to scrape targets by default
-        evaluation_interval: 15s
-    
+resource "aws_instance" "instance1" {
+  ami                    = "ami-068c0051b15cdb816"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.SG1.id]
 
-    scrape_configs:
-        - job_name: 'prometheus' # Job to scrape Prometheus's own metrics
-            static_configs:
-            - targets: ['localhost:9090'] 
-    
-    CONF
+  user_data = <<-EOF
+    #!/bin/bash
+    dnf update -y
+    dnf install -y docker
+    systemctl start docker
+    systemctl enable docker
+
+    # Create the config file
+    cat <<CONF > /home/ec2-user/prometheus.yml
+global:
+  scrape_interval: 15s
+  evaluation_interval: 15s
+
+scrape_configs:
+  - job_name: 'prometheus'
+    static_configs:
+      - targets: ['localhost:9090']
+CONF
+
+    # Run the container
     docker run -d \
-    --name my-prometheus \
-    -p 9090:9090 \
-    -v prometheus-data:/prometheus \
-    -v "$(pwd)"/prometheus.yml:/etc/prometheus/prometheus.yml \
-    prom/prometheus
-    EOF
+      --name my-prometheus \
+      -p 9090:9090 \
+      -v prometheus-data:/prometheus \
+      -v /home/ec2-user/prometheus.yml:/etc/prometheus/prometheus.yml \
+      prom/prometheus
+  EOF
 }
 
 
-resource "aws_security_group" "SG1"{
+resource "aws_security_group" "SG1" {
 
   name        = "example-security-group"
   description = "Allow HTTP and SSH access"
@@ -89,32 +95,32 @@ resource "aws_security_group" "SG1"{
 }
 
 
-resource "aws_internet_gateway" "ig"{
-    vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.main.id
 
-    tags = {
-        Name = "IGW1"
-    }
+  tags = {
+    Name = "IGW1"
+  }
 }
 
-resource "aws_route_table" "route_table"{
-    vpc_id = aws_vpc.main.id
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
 
-    route{
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.ig.id
-    }
-
-
-    route{
-        cidr_block = "10.0.0.0/16"
-        gateway_id = "local"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
 
 
-    tags = {
-        Name = "RT1"
-    }
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+
+  tags = {
+    Name = "RT1"
+  }
 }
 
 resource "aws_route_table_association" "route_table_association" {

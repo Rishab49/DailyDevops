5fd1ae63ed99363108e9a2c19bbc2d471efcd926
Rishab49 <25582966+Rishab49@users.noreply.github.com>
2026-01-05 16:28:39 +0530

    updated terraform version

--- challenge5/main.tf
+++ challenge5/main.tf
@@ -84,7 +84,6 @@ resource "aws_route_table" "rt1" {
   }
 }
 
-
 resource "aws_route_table_association" "association1" {
   subnet_id      = aws_subnet.subnet1.id
   route_table_id = aws_route_table.rt1.id

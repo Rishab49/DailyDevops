f6f98f4758de387fe70b3e4d6c83b4a58dba3887
Rishab49 <25582966+Rishab49@users.noreply.github.com>
2026-01-05 23:12:01 +0530

    updated tf

--- challenge5/main.tf
+++ challenge5/main.tf
@@ -35,7 +35,7 @@ resource "aws_subnet" "subnet2" {
 
 resource "aws_subnet" "subnet3" {
   vpc_id                  = aws_vpc.main.id
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-east-1c"
   cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = true
 }
